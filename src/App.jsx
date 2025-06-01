a45afdda0e868d8fce677b3b76cf2b3cb5d5825a
Arnold Worthy <worthyarnold@gmail.com>
2025-06-01 19:45:00 -0400

    Clean: move Vite app to root

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Elimicah Tracker</h1>
+      <p>Your job tracking system is live!</p>
+    </div>
+  );
+}
+
+export default App;
